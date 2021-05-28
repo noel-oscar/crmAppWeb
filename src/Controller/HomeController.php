965f8e1257790de9d9b60b8f4de1dc9c89e5175f
noel-oscar <noeloscar@gmail.com>
2021-05-28 15:02:04 +0200

    Created home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ * @Route "/", name="crmapp_"
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route ("/", name="home")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
